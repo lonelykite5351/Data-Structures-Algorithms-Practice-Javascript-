949cecce1a043a444d1e233d0750c79c24932c9e
lonelykite <lonelykite5351@gmail.com>
2021-12-21 00:45:10 +0800

    Update LinkedList(append, prepend)

--- LinkedList.js
+++ LinkedList.js
@@ -9,9 +9,31 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+        return this
+    }
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        }
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
 }
 
 let mylinklist = new LinkedList(10)
-console.log(mylinklist);
+// mylinklist.append(5)
+mylinklist.prepend(16)
+mylinklist
 
 // practice, not complete
