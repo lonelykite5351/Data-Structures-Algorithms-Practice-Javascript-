808ca8bb17761292b3f584b6685974ab4974c626
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2022-02-06 01:08:10 +0800

    Update Stacks Implementation(Linked List).js

--- Stacks Implementation(Linked List).js
+++ Stacks Implementation(Linked List).js
@@ -11,19 +11,42 @@ class Stack {
         this.bottom = null;
         this.length = 0;
     }
-    peek() {}
+    printStack() {
+        const array = []
+        let currentNode = this.top
+        while(currentNode) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    peek() {
+        return this.top
+    }
     push(value) {
         const newNode = new Node(value)
+        if(!this.bottom) {
+            this.bottom = newNode
+            this.top = this.bottom
+            this.length++
+            return this
+        }
+        newNode.next = this.top
+        this.top = newNode
+        this.length++
+        return this
     }
-    pop() {}
-    //isEmpty
+    pop() {
+        
+    }
 }
 
 const myStack = new Stack();
 myStack.push("Discord")
 myStack.push("Udemy")
 myStack.push("google")
-console.log(myStack);
+console.log(myStack.peek())
+console.log(myStack.printStack());
 //Discord
 //Udemy
 //google
