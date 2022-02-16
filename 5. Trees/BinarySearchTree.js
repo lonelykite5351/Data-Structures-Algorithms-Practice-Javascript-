2a3540e02bf23d84352aec33673ca0787265e888
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2022-02-16 11:58:33 +0800

    Update BinarySearchTree.js(remove)

--- 5. Trees/BinarySearchTree.js
+++ 5. Trees/BinarySearchTree.js
@@ -12,24 +12,24 @@ class BinarySearchTree {
     }
     insert(value) {
         const newNode = new Node(value)
-        if(!this.root){
+        if (!this.root) {
             this.root = newNode
             return this
         }
-        else{
+        else {
             let currentNode = this.root
-            while(true){
-                if(value < currentNode.value){
+            while (true) {
+                if (value < currentNode.value) {
                     // Left
-                    if(!currentNode.left){
+                    if (!currentNode.left) {
                         currentNode.left = newNode
                         return this
                     }
                     currentNode = currentNode.left
                 }
-                else{
+                else {
                     // Right
-                    if(!currentNode.right){
+                    if (!currentNode.right) {
                         currentNode.right = newNode
                         return this
                     }
@@ -39,33 +39,112 @@ class BinarySearchTree {
         }
     }
     lookup(value) {
-        if(!this.root){
+        if (!this.root) {
             return false
         }
 
         let currentNode = this.root
-        while(currentNode) {
-            if(value < currentNode.value){
+        while (currentNode) {
+            if (value < currentNode.value) {
                 currentNode = currentNode.left
             }
-            else if(value > currentNode.value){
+            else if (value > currentNode.value) {
                 currentNode = currentNode.right
             }
-            else if(value === currentNode.value){
+            else if (value === currentNode.value) {
                 return currentNode
             }
         }
         return false
     }
 
-    remove(value){
-        const findNode = this.lookup(value)
-        if(findNode === false) {
-            return 'No Exist Value'
+    // This remove method is from Udemy Course's solution.
+    remove(value) {
+        if (!this.root) {
+            return false;
         }
-        else{
-            if(findNode.left === null && findNode.right === null){
-                findNode = null
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            }
+            else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            }
+            else if (currentNode.value === value) {
+                //We have a match, get to work!
+
+                //Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    }
+                    else {
+
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+
+                            //if parent < current value, make left child a right child of parent
+                        }
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+
+                //Option 2: Right child which doesnt have a left child
+                }
+                else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    }
+                    else {
+
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+
+                            //if parent < current, make right child a right child of the parent
+                        }
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                    //Option 3: Right child that has a left child
+                }
+                else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    }
+                    else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        }
+                        else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
             }
         }
     }
@@ -79,6 +158,9 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
+tree.insert(180)
+tree.insert(175)
+tree.insert(160)
 console.log(tree);
 
 console.log(tree.lookup(5))
