bd006cb2d81735dac5c022b3e6cea5bece391d2a
lonelykite5351 <55124083+lonelykite5351@users.noreply.github.com>
2023-04-07 17:32:08 +0800

    Update debounce.js

--- JavaScript/debounce.js
+++ JavaScript/debounce.js
@@ -1,3 +1,21 @@
-setTimeout(() => {
-  console.log(123);
-}, 2000)
+const btn = document.querySelector('button');
+
+function payMoney(){
+  console.log("clicking");
+  console.log(this);
+}
+
+function debounce(func, delay){
+  let timer;
+  return () => {
+    // const _this = this;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // func.call(_this);
+      console.log(_this);
+    }, delay)
+  }
+}
+
+btn.addEventListener('click', debounce(payMoney, 1000));
+// btn.addEventListener('click', payMoney, 1000);
